380ed3566709db6ccd4bf6a14263a047d9d4de23
Tobias Jarmuzek <tobias.jarmuzek@rwth-aachen.de>
2013-07-19 08:37:11 +0200

    pwnypot processing class

---
+++ modules/processing/pwnypot.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2010-2013 Cuckoo Sandbox Developers.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import os
+
+from lib.cuckoo.common.abstracts import Processing
+from lib.cuckoo.common.objects import File
+
+class Pwnypot(Processing):
+    """Analysis of all files received by MCEDP.dll."""
+
+    def run(self):
+        """Run analysis.
+        @return: 
+        """
+
+        return None
